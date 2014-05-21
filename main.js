3abbfd8bbef78c80b6fdd6165667cd749e80ba33
spacegeek224 <spacegeek224@users.noreply.github.com>
2014-05-21 09:08:55 -0400

    Update main.js

--- main.js
+++ main.js
@@ -7,17 +7,17 @@ var blue = [196, 77, 55];
 var purple = [280, 50, 60];
 var letterColors = [red, orange, green, blue, purple];
 var jobValue = prompt("Enter Text"); // first element in DOM  (index 0) with name="txtJob"
-
+bubbleShape = prompt("Enter \"square\" or \"circle\" ");
 
 drawName(jobValue, letterColors);
 
 if(10 < 3)
 {
-    bubbleShape = 'square';
+    //bubbleShape = 'square';
 }
 else
 {
-    bubbleShape = 'circle';
+    //bubbleShape = 'circle';
 }
 
 bounceBubbles();
